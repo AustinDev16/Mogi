876659d431a46b0a479d1208134fa405525546c2
AustinDev16 <austin.blaser@gmail.com>
2018-01-11 17:28:18 -0700

    Added Mogi point class.

---
+++ Mogi/MogiPoint.cpp
@@ -0,0 +1,9 @@
+//
+//  MogiPoint.cpp
+//  Mogi
+//
+//  Created by Austin Blaser on 1/11/18.
+//  Copyright © 2018 Austin Blaser. All rights reserved.
+//
+
+#include "MogiPoint.hpp"
---
+++ Mogi/MogiPoint.hpp
@@ -0,0 +1,14 @@
+//
+//  MogiPoint.hpp
+//  Mogi
+//
+//  Created by Austin Blaser on 1/11/18.
+//  Copyright © 2018 Austin Blaser. All rights reserved.
+//
+
+#ifndef MogiPoint_hpp
+#define MogiPoint_hpp
+
+#include <stdio.h>
+
+#endif /* MogiPoint_hpp */
