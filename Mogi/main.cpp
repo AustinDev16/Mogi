9ad9e8a8c5d4b523b2d7027043eacfdbf2800b10
AustinDev16 <austin.blaser@gmail.com>
2018-01-11 17:05:43 -0700

    Initial Commit

---
+++ Mogi/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Mogi
+//
+//  Created by Austin Blaser on 1/11/18.
+//  Copyright © 2018 Austin Blaser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
