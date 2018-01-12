67c5a93889aeaa04372857b33055c3ac460bb60a
AustinDev16 <austin.blaser@gmail.com>
2018-01-11 18:06:59 -0700

    Added methods.

--- Mogi/MogiPoint.cpp
+++ Mogi/MogiPoint.cpp
@@ -7,3 +7,29 @@
 //
 
 #include "MogiPoint.hpp"
+
+// Public
+MogiPoint::MogiPoint(MogiPointConfig config) {
+    configuration = config;
+}
+
+double MogiPoint::calculateDeformation(double deltaP, double radialDistance) {
+    double value = mogiPointEngine(radialDistance, deltaP);
+    return value;
+}
+
+double MogiPoint::calculateDeformation(double deltaP, double x, double y) {
+    double k = convertCartesianToRadial(x, y);
+    return mogiPointEngine(k, deltaP);
+}
+
+// Private
+double MogiPoint::mogiPointEngine(double k, double dP) {
+    assert(k > 0);
+    MogiPointConfig c = configuration;
+    return ( ((1-c.poissonRatio) * dP * pow(c.chamberRadius, 3.0) )/(c.mu) ) * ((c.chamberCenterDepth)/(pow((pow(k, 2.0) + pow(c.chamberCenterDepth, 2.0)), 1.5)));
+}
+
+double MogiPoint::convertCartesianToRadial(double x, double y) {
+    return sqrt(pow(x, 2.0) + pow(y, 2.0));
+}
--- Mogi/MogiPoint.hpp
+++ Mogi/MogiPoint.hpp
@@ -10,5 +10,23 @@
 #define MogiPoint_hpp
 
 #include <stdio.h>
+#include <math.h>
+#include <assert.h>
+struct MogiPointConfig {
+    double poissonRatio; // v
+    double mu; //
+    double chamberRadius; // a, meters
+    double chamberCenterDepth; // d, meters
+};
 
+class MogiPoint {
+private:
+    MogiPointConfig configuration;
+    double mogiPointEngine(double k, double dP);
+    double convertCartesianToRadial(double x, double y);
+public:
+    MogiPoint(MogiPointConfig config);
+    double calculateDeformation(double deltaP, double radialDistance);
+    double calculateDeformation(double deltaP, double x, double y);
+};
 #endif /* MogiPoint_hpp */
